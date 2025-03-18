c5a651e36c39b3dddb4c915e2f60ba92ecc91609
Aworldc <83331712+Aworldc@users.noreply.github.com>
2025-03-18 17:07:58 +1100

    Update generate_images.py

--- generate_images.py
+++ generate_images.py
@@ -129,7 +129,8 @@ async def main() -> None:
             exclude_langs=excluded_langs,
             ignore_forked_repos=ignore_forked_repos,
         )
-        await asyncio.gather(generate_languages(s), generate_overview(s))
+        # await asyncio.gather(generate_languages(s), generate_overview(s))
+        await asyncio.gather(generate_languages(s))
 
 
 if __name__ == "__main__":
